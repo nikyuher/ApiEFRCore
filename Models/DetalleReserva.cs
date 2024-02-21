c1c6f6bbfe987c64185901ce30d1cc2f9be3c48b
nikyuher <totalgamenicogmail.com>
2024-02-21 05:24:22 +0100

    pequeños cambios para futuras cosas

--- Models/DetalleReserva.cs
+++ Models/DetalleReserva.cs
@@ -1,11 +1,12 @@
 namespace Teatro.Models;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 public class DetalleReserva
 {
     public int ReservaId { get; set; }
     public int ObraId { get; set; }
 
+    [JsonIgnore]
     public Reserva? Reserva { get; set; }
     public Obra? Obra { get; set; }
 
