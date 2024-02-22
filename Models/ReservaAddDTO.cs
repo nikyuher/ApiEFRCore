9cce602d1815a57db2fe7be9abe5d1ad2b66f97e
nikyuher <totalgamenicogmail.com>
2024-02-22 22:21:50 +0100

    Revert "V1.0"
    This reverts commit 3d18a37554e059521e3d8b1499f1c7a39a568e95.

--- Models/ReservaAddDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Teatro.Models;
-
-public class ReservaAddDTO
-{
-    public int ReservaId { get; set; }
-    public int UsuarioId { get; set; }
-    public int ObraId { get; set; }
-    public int AsientoId { get; set; }
-}
--- Models/ReservaPutDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Teatro.Models;
-
-public class ReservaPutDTO
-{
-    public int ReservaId { get; set; }
-    public int ObraId { get; set; }
-    public int AsientoId { get; set; }
-}
