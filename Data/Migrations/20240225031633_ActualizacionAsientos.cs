79c5bfa42dce4e8329aae9ab75a1db3c170ec8ee
nikyuher <totalgamenicogmail.com>
2024-02-25 04:27:00 +0100

    cambion en atributo asientos

---
+++ Data/Migrations/20240225031633_ActualizacionAsientos.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Teatro.Data.Migrations
+{
+    public partial class ActualizacionAsientos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroFila",
+                table: "Asientos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NumeroFila",
+                table: "Asientos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- Models/Asiento.cs
+++ Models/Asiento.cs
@@ -5,7 +5,6 @@ public class Asiento
     [Key]
     public int AsientoId { get; set; }
     
-    public string? NumeroFila { get; set; }
     public string? NumeroAsiento { get; set; }
     public bool? Estado { get; set; }
 
