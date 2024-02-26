f47707859c945f44531bca28891ec4ef565a911a
nikyuher <totalgamenicogmail.com>
2024-02-26 01:19:02 +0100

    Migracion de actualizacion DB

--- Data/Migrations/20240226001841_AgregarImagenesObras.Designer.cs
+++ Data/Migrations/20240226001841_AgregarImagenesObras.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Teatro.Data;
 
@@ -11,9 +12,10 @@ using Teatro.Data;
 namespace Teatro.Data.Migrations
 {
     [DbContext(typeof(TeatroContext))]
-    partial class TeatroContextModelSnapshot : ModelSnapshot
+    [Migration("20240226001841_AgregarImagenesObras")]
+    partial class AgregarImagenesObras
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
